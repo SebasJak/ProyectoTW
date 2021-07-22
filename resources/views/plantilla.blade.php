ea932d1ec1d27199e599a1efb0797702cdf6a443
SebasJak <sebastian.lukich.aste@alumnos.uta.cl>
2021-07-22 17:32:22 -0400

    Borré dos holas

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -15,15 +15,9 @@
 
     <div class="container">
         @yield('contenido')
-<<<<<<< Updated upstream
-        <p>Hola De Jose</p>
-        <p>Hola! de Sebas</p>
-
-=======
         <p>Hola de Jose!</p>
         <p>Hola! De Sebas</p>
         <p>Hola! De Seba</p>
->>>>>>> Stashed changes
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
