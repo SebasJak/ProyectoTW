c7d9dbd19f9c33cadeb2cea7e8c6b381624aecf3
SebasJak <sebastian.lukich.aste@alumnos.uta.cl>
2021-07-22 17:18:17 -0400

    Inserte un hola
    MI hola es mejor a los otro holas

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -16,7 +16,8 @@
     <div class="container">
         @yield('contenido')
         <p>Hola De Jose</p>
-        
+        <p>Hola! de Sebas</p>
+
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
